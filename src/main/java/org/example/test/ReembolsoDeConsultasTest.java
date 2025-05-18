369025154da7de512e5dc4cbf46def18f8ea3740
Matheus Zilli <matheuszilli@outlook.com>
2025-05-18 20:10:19 -0300

    Exercicio 02 - Implementação de testes com 0% e 100% de reembolso

--- src/main/java/org/example/test/ReembolsoDeConsultasTest.java
+++ src/main/java/org/example/test/ReembolsoDeConsultasTest.java
@@ -12,8 +12,23 @@ public class ReembolsoDeConsultasTest {
     public void calcularReembolsoDeConsultaCom70PorCentoDeCobertura() {
         ReembolsoDeConsultas reembolso = new ReembolsoDeConsultas();
         double resultado = reembolso.calcular(200.0, 70.0);
-        double esperado = 140.0;
-        assertEquals(esperado, resultado, 0.01);
+        double reembolsoEsperado = 140.00;
+        assertEquals(reembolsoEsperado, resultado, 0.01);
+    }
 
+    @Test
+    public void calcularReembolsoDeConsultaCom0PorCentoDeCobertura() {
+        ReembolsoDeConsultas reembolso = new ReembolsoDeConsultas();
+        double resultado = reembolso.calcular(100.0, 0.0);
+        double reembolsoEsperado = 0.00;
+        assertEquals(reembolsoEsperado, resultado, 0.01);
+    }
+
+    @Test
+    public void calcularReembolsoDeConsultaCom100PorCentoDeCobertura() {
+        ReembolsoDeConsultas reembolso = new ReembolsoDeConsultas();
+        double resultado = reembolso.calcular(100.0, 100.0);
+        double reembolsoEsperado = 100.0;
+        assertEquals(reembolsoEsperado, resultado, 0.01);
     }
 }
