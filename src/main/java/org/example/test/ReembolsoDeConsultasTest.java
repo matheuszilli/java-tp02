6a14b0710367f7c106eabe3433d97e967acbf272
Matheus Zilli <matheuszilli@outlook.com>
2025-05-18 19:47:22 -0300

    Exercicio 01 - Utilizando metodologia TDD foi implementando um Teste para algo que ainda não existe. Teste está falhando

---
+++ src/main/java/org/example/test/ReembolsoDeConsultasTest.java
@@ -0,0 +1,18 @@
+package org.example.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReembolsoDeConsultasTest {
+
+
+    @Test
+    public void calcularReembolsoDeConsultaCom70PorCentoDeCobertura {
+        ReembolsoDeConsultas reembolso = new ReembolsoDeConsultas();
+        double resultado = reembolso.calcular(200.0, 70.0);
+        double esperado = 140.0;
+        assertEquals(esperado, resultado, 0.01);
+
+    }
+}
