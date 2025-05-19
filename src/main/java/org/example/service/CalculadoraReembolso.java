6f9f9519163cb1123c524f18b845ce1be8827cd9
Matheus Zilli <matheuszilli@outlook.com>
2025-05-18 22:40:41 -0300

    Exercicio 04 - Criado classe paciente, e no teste usado um dummy criado em private Paciente para conseguir referenciar em cada teste

--- src/main/java/org/example/service/CalculadoraReembolso.java
+++ src/main/java/org/example/service/CalculadoraReembolso.java
@@ -1,7 +1,9 @@
 package org.example.service;
 
+import org.example.model.Paciente;
+
 public class CalculadoraReembolso {
-    public double calcular(double valorConsulta, double percentualReembolso) {
+    public double calcular(Paciente paciente, double valorConsulta, double percentualReembolso) {
         return valorConsulta * (percentualReembolso / 100);
     }
 }
--- src/main/java/org/example/test/ReembolsoDeConsultasTest.java
+++ src/main/java/org/example/test/ReembolsoDeConsultasTest.java
@@ -1,5 +1,6 @@
 package org.example.test;
 
+import org.example.model.Paciente;
 import org.example.service.CalculadoraReembolso;
 import org.junit.Test;
 
@@ -10,25 +11,32 @@ public class ReembolsoDeConsultasTest {
 
     @Test
     public void calcularReembolsoDeConsultaCom70PorCentoDeCobertura() {
+        Paciente paciente = criarPacienteDummy();
         CalculadoraReembolso reembolso = new CalculadoraReembolso();
-        double resultado = reembolso.calcular(200.0, 70.0);
+        double resultado = reembolso.calcular(paciente, 200.0, 70.0);
         double reembolsoEsperado = 140.00;
         assertEquals(reembolsoEsperado, resultado, 0.01);
     }
 
     @Test
     public void calcularReembolsoDeConsultaCom0PorCentoDeCobertura() {
+        Paciente paciente = criarPacienteDummy();
         CalculadoraReembolso reembolso = new CalculadoraReembolso();
-        double resultado = reembolso.calcular(100.0, 0.0);
+        double resultado = reembolso.calcular(paciente, 100.0, 0.0);
         double reembolsoEsperado = 0.00;
         assertEquals(reembolsoEsperado, resultado, 0.01);
     }
 
     @Test
     public void calcularReembolsoDeConsultaCom100PorCentoDeCobertura() {
+        Paciente paciente = criarPacienteDummy();
         CalculadoraReembolso reembolso = new CalculadoraReembolso();
-        double resultado = reembolso.calcular(100.0, 100.0);
+        double resultado = reembolso.calcular(paciente, 100.0, 100.0);
         double reembolsoEsperado = 100.0;
         assertEquals(reembolsoEsperado, resultado, 0.01);
+    }
+
+    private Paciente criarPacienteDummy() {
+        return new Paciente("Matheus Dummy", "123456789-70", "matheus.zilli@al.infnet.edu.br");
     }
 }
